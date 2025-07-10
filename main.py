9e791af8bf97ebb91fe2046f4585506942ea5f8d
Elliot Challenger <elliotchallenger1@gmail.com>
2025-07-10 22:26:59 +1200

    added dewpoint

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 import os
+import math
 from datetime import datetime, timezone
 from dotenv import load_dotenv
 from urllib.parse import quote
 
 # === CONFIG ===
-SWITCHBOT_TOKEN = os.environ("SWITCHBOT_TOKEN")
-DEVICE_ID = os.environ("DEVICE_ID")
-WOW_SITE_ID = os.environ("WOW_SITE_ID")
-WOW_API_KEY = os.environ("WOW_API_KEY")
+SWITCHBOT_TOKEN = os.getenv("SWITCHBOT_TOKEN")
+DEVICE_ID = os.getenv("DEVICE_ID")
+WOW_SITE_ID = os.getenv("WOW_SITE_ID")
+WOW_API_KEY = os.getenv("WOW_API_KEY")
 
 # === Step 1: Fetch data from SwitchBot ===
 def get_switchbot_data():
@@ -18,13 +19,19 @@ def get_switchbot_data():
     url = f'https://api.switch-bot.com/v1.1/devices/{DEVICE_ID}/status'
     response = requests.get(url, headers=headers)
     data = response.json()['body']
-    temperature = data.get('temperature')
+    temperature = data.get('temperature') #temp in C
+    humidity = data.get('humidity') #RH
+    # Constants for Magnus formula
+    a = 17.27
+    b = 237.7
+    alpha = ((a * temperature) / (b + temperature)) + math.log(humidity / 100.0)
+    dewpoint_celsius = (b * alpha) / (a - alpha)
+    dewpoint = (dewpoint_celsius * 9/5) + 32
     temperature = (temperature * 9/5) + 32
-    humidity = data.get('humidity')
-    return temperature, humidity
+    return temperature, humidity, dewpoint
 
 # === Step 2: Format data for WOW ===
-def format_wow_payload(temp, humid):
+def format_wow_payload(temp, humid, dewpt):
     timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     encoded_timestamp = quote(timestamp)
     payload = {
@@ -33,6 +40,7 @@ def format_wow_payload(temp, humid):
         'dateutc': encoded_timestamp,
         'tempf': temp,
         'humidity': humid,
+        'dewptf' : dewpt,
         'softwaretype':"SwitchBot2MetOfficeWOW"
     }
     return payload
@@ -46,6 +54,6 @@ def upload_to_wow(payload):
 
 # === Main Routine ===
 if __name__ == '__main__':
-    temp, humid = get_switchbot_data()
-    wow_payload = format_wow_payload(temp, humid)
+    temp, humid, dewpt = get_switchbot_data()
+    wow_payload = format_wow_payload(temp, humid, dewpt)
     upload_to_wow(wow_payload)
